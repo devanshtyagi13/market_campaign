c62e23d064a031bf87b649ee35727b102ee43abb
devanshtyagi13 <66518032+devanshtyagi13@users.noreply.github.com>
2024-07-07 15:53:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 import oracledb
-# import requests
+import random
+from faker import Faker
+ 
  
 app = Flask(__name__)
+faker = Faker()
  
 def get_db_connection():
     # Update these values with your actual database credentials
     dsn = 'localhost:1521/xepdb1'
     conn = oracledb.connect(user="dbuser", password="dbuser", dsn=dsn)
     return conn
- 
  
 @app.route('/')
 def index():
@@ -21,61 +23,86 @@ def index():
     tables = [row[0] for row in cursor.fetchall( )]
     cursor.close()
     conn.close()
-    return render_template('index.html', tables = tables)
-   
-@app.route('/table/<table_name>')
-def view_table(table_name):
+    return render_template('base.html', tables = tables)
+ 
+ 
+@app.route('/filtered_customers1')
+def filtered_customers1():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT table_name FROM user_tables")
-    query = f"SELECT * FROM {table_name}"
- 
-    cursor.execute(query)
-    rows = cursor.fetchall()
+    table='customer_bank_details'
+    table2='customer_campaign_data'
+    cursor.execute(f"SELECT * FROM {table} INNER JOIN {table2} USING(customer_id) where {'loan'} =: x and {'campaign'}> 2", {'x':'yes'})
+    row=cursor.fetchall()
     columns = [desc[0] for desc in cursor.description]
-    return render_template('info.html', column_names= columns, rows=rows,table_name=table_name)
+    return render_template('filtered_customers1.html', columns=columns,rows=row)
  
- 
-@app.route('/edit/<table_name>/<string:row_id>', methods=['GET', 'POST'])
-def edit_row(table_name, row_id):
-    if request.method == 'POST':
-        # Process form submission and update row in the database
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        query = f"SELECT * FROM {table_name}"
-        cursor.execute(query)
-        columns = [desc[0] for desc in cursor.description]
-        update_query = f"UPDATE {table_name} SET "
-        update_params = []
-        for key, value in request.form.items():
-            if key != 'submit':
-                column_name = key
-                update_query += f"{column_name} = :{column_name}, "
-                update_params.append(value)
-        update_query = update_query.rstrip(', ') + f" WHERE {columns[0]} = '{row_id}'"
-        '''print(update_query)
-        print(update_params)'''
-        cursor.execute(update_query, update_params)
-        connection.commit()
-        cursor.close()
-       
-        return('/')
-   
-    # Fetch row data for editing
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    query = f"SELECT * FROM {table_name}"
-    cursor.execute(query)
+@app.route('/filtered_customers2')
+def filtered_customers2():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    table='customer_bank_details'
+    table2='customer_campaign_data'
+    cursor.execute(f"select * from {table} INNER JOIN {table2} USING(customer_id) where {'campaign'} >2 and {'poutcome'} ='success'")
+    row=cursor.fetchall()
     columns = [desc[0] for desc in cursor.description]
-    index_row=columns[0]
-    cursor.close()  
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM {table_name} WHERE {index_row} = :row_id", {'row_id': row_id})
-    row = cursor.fetchone()
-    cursor.close()
+    data=[]
+    for i in row:
+        con_list=list(i)
+        data1=dict(zip(columns,con_list))
+        data.append(data1)
+    return render_template('filtered_customers2.html', filtered_customers=data,columns=columns,rows=row)
+ 
+@app.route('/filtered_customers3')
+def filtered_customers3():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    table='customer_bank_details'
+    table2='customer_campaign_data'
+    cursor.execute(f"select * from {table} INNER JOIN {table2} USING(customer_id) where {'campaign'} >2 and {'poutcome'} ='failure' and {'month'}='jun' ")
+    row=cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    data=[]
+    for i in row:
+        con_list=list(i)
+        data1=dict(zip(columns,con_list))
+        data.append(data1)
+    return render_template('filtered_customers3.html', filtered_customers=data,columns=columns,rows=row)
+ 
+@app.route('/filtered_customers4')
+def filtered_customers4():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    table='customer_bank_details'
+    table2='customer_campaign_data'
+    cursor.execute(f"select state_code,count(Customer_id) from {table} group by {'state_code'} order by {'state_code'}")
+    row=cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    data=[]
+    for i in row:
+        con_list=list(i)
+        data1=dict(zip(columns,con_list))
+        data.append(data1)
+    return render_template('filtered_customers4.html', filtered_customers=data,columns=columns,rows=row)
+ 
+@app.route('/getDetails')
+def getDetails():
+    return render_template('name.html')
+ 
+@app.route('/details',methods=['POST'])
+def search(table='customer_details'):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    customer_id = request.form.get('customer_id')
+    #print(customer_name)
+    row_id='Customer_id'
+    cursor.execute(f"SELECT * FROM {table} WHERE {row_id}= :row_id", {'row_id':customer_id})
+    row=cursor.fetchone()
+    columns = [desc[0] for desc in cursor.description]
     row=list(row)
     dic=dict(zip(columns,row))
-    return render_template('edittable.html', table_name=table_name, dic=dic)
+    print(dic)
+    return render_template('display.html',columns_rows=dic)
  
 if __name__ == '__main__':
     app.run(debug=True)
